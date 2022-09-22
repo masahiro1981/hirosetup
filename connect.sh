0f4c5da2a55c001c2cf5f4b41f18f5d660ac3290
Masahiro Nakagawa <masahiro.nakagawa@canonical.com>
2022-09-22 04:50:19 +0000

    Fix mistake in interface hook. Add script to connect all gpios

---
+++ connect.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+snap connect hirosetup:qcs-gpio-412 mic-qcs410:qcs-gpio-412
+snap connect hirosetup:qcs-gpio-413 mic-qcs410:qcs-gpio-413
+snap connect hirosetup:qcs-gpio-438 mic-qcs410:qcs-gpio-438
+snap connect hirosetup:qcs-gpio-450 mic-qcs410:qcs-gpio-450
+snap connect hirosetup:qcs-gpio-452 mic-qcs410:qcs-gpio-452
+snap connect hirosetup:qcs-gpio-456 mic-qcs410:qcs-gpio-456
+snap connect hirosetup:qcs-gpio-461 mic-qcs410:qcs-gpio-461
+snap connect hirosetup:qcs-gpio-462 mic-qcs410:qcs-gpio-462
+snap connect hirosetup:qcs-gpio-463 mic-qcs410:qcs-gpio-463
+snap connect hirosetup:qcs-gpio-464 mic-qcs410:qcs-gpio-464
+snap connect hirosetup:qcs-gpio-465 mic-qcs410:qcs-gpio-465
+snap connect hirosetup:qcs-gpio-468 mic-qcs410:qcs-gpio-468
+snap connect hirosetup:qcs-gpio-470 mic-qcs410:qcs-gpio-470
+snap connect hirosetup:qcs-gpio-473 mic-qcs410:qcs-gpio-473
+snap connect hirosetup:qcs-gpio-477 mic-qcs410:qcs-gpio-477
+snap connect hirosetup:qcs-gpio-479 mic-qcs410:qcs-gpio-479
+snap connect hirosetup:qcs-gpio-480 mic-qcs410:qcs-gpio-480
+snap connect hirosetup:qcs-gpio-481 mic-qcs410:qcs-gpio-481
+snap connect hirosetup:qcs-gpio-482 mic-qcs410:qcs-gpio-482
+snap connect hirosetup:qcs-gpio-488 mic-qcs410:qcs-gpio-488
+snap connect hirosetup:qcs-gpio-492 mic-qcs410:qcs-gpio-492
+snap connect hirosetup:qcs-gpio-493 mic-qcs410:qcs-gpio-493
+snap connect hirosetup:qcs-gpio-502 mic-qcs410:qcs-gpio-502
+
